088e7085ee44c0038a8e2d235f9071aa4756a555
bijoymalaker <bijoymalaker7@gmail.com>
2025-03-10 23:42:51 +0600

    portfolio page ready

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,151 +1,136 @@
 <template>
-    <article class="portfolio active" data-page="portfolio">
-<header>
-  <h2 class="h2 article-title">Portfolio</h2>
-</header>
+  <article class="portfolio active" data-page="portfolio">
+    <header>
+      <h2 class="h2 article-title">Portfolio</h2>
+    </header>
 
-<section class="projects">
+    <section class="projects">
 
-  <ul class="filter-list">
+      <ul class="filter-list">
 
-    <li class="filter-item">
-      <button class="active" data-filter-btn>All</button>
-    </li>
+        <li class="filter-item" v-for="(category, index) in Categories" :key="index">
+          <button :class="{ active: category === selectedCategory }" @click="selectedCategory = category">{{ category
+            }}</button>
+        </li>
 
-    <li class="filter-item">
-      <button data-filter-btn>Web design</button>
-    </li>
+      </ul>
 
-    <li class="filter-item">
-      <button data-filter-btn>Applications</button>
-    </li>
+      <div class="filter-select-box">
 
-    <li class="filter-item">
-      <button data-filter-btn>Web development</button>
-    </li>
+        <button class="filter-select" data-select>
 
-  </ul>
+          <div class="select-value" data-selecct-value>Select category</div>
 
-  <div class="filter-select-box">
+          <div class="select-icon">
+            <ion-icon name="chevron-down"></ion-icon>
+          </div>
 
-    <button class="filter-select" data-select>
+        </button>
 
-      <div class="select-value" data-selecct-value>Select category</div>
+        <ul class="select-list">
 
-      <div class="select-icon">
-        <ion-icon name="chevron-down"></ion-icon>
+          <li class="select-item" v-for="(category, index) in Categories" :key="index">
+            <button :class="{ active: category === selectedCategory }">{{ category }}</button>
+          </li>
+
+
+        </ul>
+
       </div>
 
-    </button>
+      <ul class="project-list">
 
-    <ul class="select-list">
+        <li class="project-item active" v-for="project in filterProject" :key="project.id">
+          <a :href="project.link">
 
-      <li class="select-item">
-        <button data-select-item>All</button>
-      </li>
+            <figure class="project-img">
+              <div class="project-item-icon-box">
+                <ion-icon name="eye-outline"></ion-icon>
+              </div>
 
-      <li class="select-item">
-        <button data-select-item>Web design</button>
-      </li>
+              <img :src="project.img" :alt="project.title" loading="lazy">
+            </figure>
 
-      <li class="select-item">
-        <button data-select-item>Applications</button>
-      </li>
+            <h3 class="project-title">{{ project.title }}</h3>
 
-      <li class="select-item">
-        <button data-select-item>Web development</button>
-      </li>
+            <p class="project-category">{{ project.category }}</p>
 
-    </ul>
+          </a>
+        </li>
 
-  </div>
 
-  <ul class="project-list">
+      </ul>
 
-    <li class="project-item  active" data-filter-item data-category="web development">
-      <a href="https://quicckbitee.netlify.app/">
+    </section>
 
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline"></ion-icon>
-          </div>
-
-          <img src="./assets/images/project-1.jpg" alt="finance" loading="lazy">
-        </figure>
-
-        <h3 class="project-title">QuickBite</h3>
-
-        <p class="project-category">Web development</p>
-
-      </a>
-    </li>
-
-    <li class="project-item  active" data-filter-item data-category="web development">
-      <a href="https://prettypicks.netlify.app/">
-
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline"></ion-icon>
-          </div>
-
-          <img src="./assets/images/project-2.png" alt="orizon" loading="lazy">
-        </figure>
-
-        <h3 class="project-title">Pretty Picks</h3>
-
-        <p class="project-category">Web development</p>
-
-      </a>
-    </li>
-
-    <li class="project-item  active" data-filter-item data-category="web design">
-      <a href="https://bijoymalaker.github.io/doin-website/">
-
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline"></ion-icon>
-          </div>
-
-          <img src="./assets/images/project-3.jpg" alt="fundo" loading="lazy">
-        </figure>
-
-        <h3 class="project-title">Doin Website</h3>
-
-        <p class="project-category">Web design</p>
-
-      </a>
-    </li>
-
-    <li class="project-item  active" data-filter-item data-category="applications">
-      <a href="https://bijoymalaker.github.io/project_burger/">
-
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline"></ion-icon>
-          </div>
-
-          <img src="./assets/images/project-4.png" alt="brawlhalla" loading="lazy">
-        </figure>
-
-        <h3 class="project-title">Project Burger</h3>
-
-        <p class="project-category">Applications</p>
-
-      </a>
-    </li>
-
-    
-  </ul>
-
-</section>
-
-</article>
+  </article>
 </template>
 <script>
+
+import project1 from "../assets/images/project1.jpg"
+import project2 from "../assets/images/project2.jpg"
+import project3 from "../assets/images/project3.jpg"
+import project4 from "../assets/images/project4.jpg"
+import project5 from "../assets/images/project5.jpg"
+
+
+
 export default {
 
+
+  name: 'Portfolio',
+  data() {
+    return {
+      selectedCategory: "All",
+      Categories: ["All", "Web Development", "Web Design", "Applications"],
+      Projects: [
+        {
+          id: 1,
+          title: "QuickBite",
+          category: "Web Development",
+          img: project1,
+          link: "https://quicckbitee.netlify.app/"
+        },
+        {
+          id: 2,
+          title: "Pretty Picks",
+          category: "Web Development",
+          img: project2,
+          link: "https://prettypicks.netlify.app/"
+        },
+        {
+          id: 3,
+          title: "Doin Website",
+          category: "Web Design",
+          img: project3,
+          link: "https://bijoymalaker.github.io/doin-website/"
+        },
+        {
+          id: 4,
+          title: "Project Burger",
+          category: "Applications",
+          img: project4,
+          link: "https://bijoymalaker.github.io/project_burger/"
+        },
+        {
+          id: 4,
+          title: "Simple Project",
+          category: "Web Design",
+          img: project5,
+          link: "#"
+        },
+      ]
+    }
+  },
+  computed: {
+    filterProject() {
+      if (this.selectedCategory === "All") {
+        return this.Projects;
+      } else {
+        return this.Projects.filter(item => item.category === this.selectedCategory);
+      }
+    }
+  }
 }
 </script>
-<style>
-
-</style>
+<style></style>
