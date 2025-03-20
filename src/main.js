4f1642c5212ca909d96f4c1f11e27ed04dd7f622
bijoymalaker <bijoymalaker7@gmail.com>
2025-03-21 00:22:57 +0600

    side nav bug not fixed

--- src/main.js
+++ src/main.js
@@ -11,13 +11,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faPhone, faCakeCandles, faLocationDot, faUserGraduate, faBookBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faCakeCandles, faLocationDot, faUserGraduate, faBookBookmark, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import {  } from '@fortawesome/free-brands-svg-icons'
 import { faEye, faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 
 
 /* add icons to the library */
-library.add(faEye,faEnvelope, faPhone, faCakeCandles, faLocationDot, faUserGraduate, faBookBookmark)
+library.add(faEye,faEnvelope, faPhone, faCakeCandles, faLocationDot, faUserGraduate, faBookBookmark, faChevronDown)
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
